fda991f7d8de08fb72f14b0e4eb468779e2af38e
Jan Groot <j.j.groot@gmail.com>
2019-12-11 17:06:05 +0100

    day 1o test enabled

--- src/test/java/io/oac/contest/Day10Test.java
+++ src/test/java/io/oac/contest/Day10Test.java
@@ -1,13 +1,11 @@
 package io.oac.contest;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
-@Disabled
 public class Day10Test {
 
     @ParameterizedTest
